18ee67e04f370cb2d6d7d65744f2a53c25ef2a0c
Ademola Oladapo <ademolaoladapo@gmail.com>
2021-10-08 10:52:08 +0000

    reorganization

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+"""
+solution 1
 if direction == 'encode':
 
   def encrypt(plain_text, shift_amount):
@@ -18,15 +19,54 @@ if direction == 'encode':
   encrypt(plain_text=text, shift_amount=shift)
 else:
   def decrypt(cipher_text, shift_amount):
-    decryption = ''
+    plain_text = ''
     for letter in cipher_text:
       position = alphabet.index(letter)
       new_position = position - shift
       new_letter = alphabet[new_position]
       decryption +=new_letter
-    print(f'The decripted text is {decryption}')
+    print(f'The decoded text is {plain_text}')
   decrypt(cipher_text=text, shift_amount=shift)
+"""
+'''
+solution2
+def encrypt_decrypt(text, shift, direction):
+  if direction == 'encode':
+ 
+    cipher_text = ''
+    for letter in text:
+      position = alphabet.index(letter)
+      new_position = position + shift
+      new_letter = alphabet[new_position]
+      cipher_text += new_letter
+    print(f'The encoded text is {cipher_text}')
+  else:
+    direction == 'decode'
+    plain_text = ''
+    for letter in text:
+      position = alphabet.index(letter)
+      new_position = position - shift
+      new_letter = alphabet[new_position]
+      plain_text +=new_letter
+    print(f'The decoded text is {plain_text}')
 
+encrypt_decrypt(text, shift, direction)
+'''
+
+'''solution3'''
+def caeser(start_text, shift_amount, cipher_direction):
+  end_text = ''
+
+  if cipher_direction == 'decode':
+    shift_amount *=-1
+
+  for letter in start_text:
+    position = alphabet.index(letter)
+    new_position = position + shift_amount
+    end_text += alphabet[new_position]
+  print(f'The {cipher_direction}d text is {end_text}')
+
+caeser(start_text=text, shift_amount=shift, cipher_direction=direction)
   
    
 
