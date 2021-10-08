00019feb46d1b6a68f59424774beac6a86d9607f
Ademola Oladapo <ademolaoladapo@gmail.com>
2021-10-08 08:42:45 +0000

    improve the code to also decript(decode) cipher

--- main.py
+++ main.py
@@ -5,15 +5,28 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(plain_text, shift_amount):
-  cipher_text = ''
-  for letter in text:
-    position = alphabet.index(letter)
-    new_position = position + shift
-    new_letter = alphabet[new_position]
-    cipher_text += new_letter
-  print(f'The encoded text is {cipher_text}')
+if direction == 'encode':
+
+  def encrypt(plain_text, shift_amount):
+    cipher_text = ''
+    for letter in text:
+      position = alphabet.index(letter)
+      new_position = position + shift
+      new_letter = alphabet[new_position]
+      cipher_text += new_letter
+    print(f'The encoded text is {cipher_text}')
+  encrypt(plain_text=text, shift_amount=shift)
+else:
+  def decrypt(cipher_text, shift_amount):
+    decryption = ''
+    for letter in cipher_text:
+      position = alphabet.index(letter)
+      new_position = position - shift
+      new_letter = alphabet[new_position]
+      decryption +=new_letter
+    print(f'The decripted text is {decryption}')
+  decrypt(cipher_text=text, shift_amount=shift)
 
   
    
-encrypt(plain_text=text, shift_amount=shift)
+
