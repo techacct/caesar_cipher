edd3b196c1a22ffb75c60116efd1a4c8afcf2fdd
Ademola Oladapo <ademolaoladapo@gmail.com>
2021-10-21 08:23:11 +0000

    fine tuned

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+
 
 """
 solution 1
@@ -60,13 +58,25 @@ def caeser(start_text, shift_amount, cipher_direction):
   if cipher_direction == 'decode':
     shift_amount *=-1
 
-  for letter in start_text:
-    position = alphabet.index(letter)
-    new_position = position + shift_amount
-    end_text += alphabet[new_position]
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
   print(f'The {cipher_direction}d text is {end_text}')
+restart = True
 
-caeser(start_text=text, shift_amount=shift, cipher_direction=direction)
+while restart:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  shift = shift % 26
+
+  caeser(start_text=text, shift_amount=shift, cipher_direction=direction)
+  question = input('Do you want to go again? s or no')
   
    
 
